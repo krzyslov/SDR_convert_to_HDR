c61ba3df4664a277aa889385b3496e13f7d84a75
krzyslov <bbenuss@onet.pl>
2024-11-05 22:00:37 +0100

    Sample Video read and some bug fixes

--- main.cpp
+++ main.cpp
@@ -3,6 +3,29 @@
 
 int main()
 {
+    cv::VideoCapture reader("SDR_sample_footage.mp4");
+    if (!reader.isOpened()) {
+        std::cerr << "Cannot load video!" << std::endl;
+        return -1;
+    }
+
+    cv::Mat SDR_frame;
+    while(true){
+
+        bool isSuccess = reader.read(SDR_frame);
+        if (!isSuccess) {
+            break;
+        } else {
+        cv::imshow("SDR FRAME", SDR_frame);
+        }
+
+        if (cv::waitKey(10) == 27) { // Press ESC to quit
+            break;
+        }
+    }
+
+    reader.release();
+    cv::destroyAllWindows();
 
     return 0;
 }
