83160e7804923139a225e4c0a85591ed58e73f47
krzyslov <bbenuss@onet.pl>
2024-11-05 22:45:37 +0100

    implementation of convertSDRtoHDR without SIMD

--- main.cpp
+++ main.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-
-// convert function template
 cv::Mat convertSDRtoHDR(const cv::Mat& frame)
 {
-    return frame;
+    cv::Mat hdrFrame;
+
+    frame.convertTo(hdrFrame, CV_32F, 1.0 / 255.0); // Normalization to 0-1
+
+    float gamma = 2.2f; // Gamma corection factor
+
+    int numPixels = hdrFrame.rows * hdrFrame.cols * hdrFrame.channels();
+
+    float* pixelPtr = reinterpret_cast<float*>(hdrFrame.data);
+
+    for (int i = 0; i < numPixels; ++i) {
+        pixelPtr[i] = std::log(std::max(pixelPtr[i], 1e-8f)); // Avoiding log(0)
+        pixelPtr[i] = std::exp(gamma * pixelPtr[i]);
+    }
+
+    // Factor for Brightness range scaling for HDR
+    float hdrScale = 1.5f;
+    // Scaling
+    cv::multiply(hdrFrame, hdrScale, hdrFrame);
+
+    // Limiting pixel values ​​to 0-1 (to avoid excessive brightness)
+    cv::threshold(hdrFrame, hdrFrame, 1.0, 1.0, cv::THRESH_TRUNC);
+
+    cv::Mat hdrFrame8U;
+    hdrFrame.convertTo(hdrFrame8U, CV_8U, 255); 
+
+    return hdrFrame8U;
 }
 
 int main()
